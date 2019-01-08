1f3aab02011bf310e2192d7613aa9cd3dfc49ebe
mambotango3000 <mambotango3000@gmail.com>
2019-01-08 10:32:43 -0500

    no dist folder without aot

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.static(__dirname + '/dist'));
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 
