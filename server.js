d656782bcb08c0a4fc75afa54419cd722faac888
mambotango3000 <mambotango3000@gmail.com>
2019-01-08 10:57:48 -0500

    spitballing

--- server.js
+++ server.js
@@ -25,12 +25,12 @@ app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/'));
+app.use(express.static('/'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/'));
+  res.sendFile(path.join('/comics'));
 });
 
 
